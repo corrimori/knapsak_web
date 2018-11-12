2a140be1c1ff2ffa8a7f22c2f6393751d5d7ebc5
corrimori <corrine@promptt.com>
2018-11-12 12:04:01 -0800

    add items from backend to display

--- public/scripts.js
+++ public/scripts.js
@@ -1,4 +1,4 @@
-let apiUrl = 'http://localhost:8010/knapsaks'
+let apiUrl = 'http://localhost:8010'
 
 const loadKnapsaks = () => {
   console.log('loading knapsaks...')
@@ -6,7 +6,7 @@ const loadKnapsaks = () => {
   let knskList = document.querySelector('#knapsakLists')
   knskList.innerHTML = ''
 
-  axios.get(apiUrl).then(result => {
+  axios.get(apiUrl + '/knapsaks').then(result => {
     console.log('result.data------->', result.data)
     let data = result.data
     console.log('result.data[0]>>>', result.data[0]);
@@ -27,3 +27,62 @@ const loadKnapsaks = () => {
 } // end function loadKnapsaks
 
 loadKnapsaks()
+
+const loadItems = () => {
+  console.log('loading items...')
+
+  let itemsList = document.querySelector('#items-container')
+
+  axios.get(apiUrl + '/items').then(result => {
+    console.log('result.data ITEMS -------->', result.data);
+    console.log('name', result.data[3].name);
+    let itemData = result.data
+    itemData.forEach( element => {
+      // create a card from each element
+      let itemCard = `
+      <div class="card d-flex flex-row" style="width: 55rem;">
+              <img src="./img/${element.itemImage}" width="150px" height="150px" alt="item">
+              <div class="card-body">
+                <h5 class="card-title">${element.name}</h5>
+                <br>
+
+                <div class="qtyIncButton d-inline-flex p-6">
+                  <button type="button" class="btn btn-default btn-number btn-outline-secondary" onclick="${minus}">
+                    <span class="fas fa-minus"></span>
+                  </button>
+
+                  <input type="text" class="form-control input-number col-md-2" value="${count}" class="count">
+
+                  <button type="button" class="btn btn-default btn-number btn-outline-secondary" onclick="plus()">
+                    <span class="fas fa-plus"></span>
+                  </button>
+
+                </div>
+              </div>
+            </div>
+      `
+      let newItemCard = document.createElement('div')
+      newItemCard.innerHTML = itemCard
+      itemsList.appendChild(newItemCard)
+    })
+  })
+}
+
+loadItems()
+
+// increment counter
+
+let count = 1;
+let counter = document.querySelectorAll(".count");
+function plus(){
+  console.log('event>>', event);
+    count++;
+    event.target.value = count;
+    console.log('count', count);
+}
+function minus(){
+  if (count > 1) {
+    count--;
+    counter.value = count;
+  }
+}

0e1dd4dc52fb864008963458da0a274357a94886
corrimori <corrine@promptt.com>
2018-11-12 16:47:36 -0800

    add qty counter to each item

--- public/scripts.js
+++ public/scripts.js
@@ -37,7 +37,13 @@ const loadItems = () => {
     console.log('result.data ITEMS -------->', result.data);
     console.log('name', result.data[3].name);
     let itemData = result.data
+    let qtyObj = {}
+
     itemData.forEach( element => {
+      // create an object to store the quanity of each item
+      qtyObj[`${element.id}`] = 1
+      console.log('qtyobj>>>', qtyObj);
+
       // create a card from each element
       let itemCard = `
         <div class="card d-flex flex-row" style="width: 55rem;">
@@ -47,13 +53,14 @@ const loadItems = () => {
             <br>
 
             <div class="qtyIncButton d-inline-flex p-6">
-              <button type="button" class="btn btn-default btn-number btn-outline-secondary" onclick="${minus}">
+
+              <button type="button" class="btn btn-default btn-number btn-outline-secondary" id="${element.name}-minus-btn">
                 <span class="fas fa-minus"></span>
               </button>
 
-              <input type="text" class="form-control input-number col-md-2" id="${element.id}" value="${count}" class="count">
+              <input type="text" class="form-control input-number col-md-2" id="qty-${element.name}" value="0">
 
-              <button type="button" class="btn btn-default btn-number btn-outline-secondary" onclick="plus(${element.id})">
+              <button type="button" class="btn btn-default btn-number btn-outline-secondary" id="${element.name}-plus-btn">
                 <span class="fas fa-plus"></span>
               </button>
 
@@ -65,10 +72,23 @@ const loadItems = () => {
       let newCard = document.createElement('div')
       newCard.innerHTML = itemCard
       itemsList.appendChild(newCard)
-      console.log('el id>>>', element.id);
-      console.log('el>>>', element);
 
-      // console.log('input#>>>', input);
+      // minus qty from input value button
+      let minusButton = document.querySelector(`#${element.name}-minus-btn`)
+
+      minusButton.addEventListener("click", () => {
+        let count = document.querySelector(`#qty-${element.name}`).value
+        if (count > 0)
+        document.querySelector(`#qty-${element.name}`).value--
+      })
+
+      // plus qty from input value button
+      let plusButton = document.querySelector(`#${element.name}-plus-btn`)
+
+      plusButton.addEventListener("click", () => {
+        document.querySelector(`#qty-${element.name}`).value++
+      })
+
     })
   })
 }
@@ -77,16 +97,25 @@ loadItems()
 
 // increment counter
 
-let count = 1;
+// let count = 1;
 // let counter = document.querySelector("${elId}");
 
-const plus = (elId) => {
+const plus = (elId, qtyObj) => {
   console.log('event>>', event);
-  console.log('element id>>', elId)
-  let tempCtn = document.querySelector("elId")
-  tempCtn.innerHTML = '&'
+  console.log('el id>>', elId)
+  console.log('qtyObj in plus>>>>', qtyObj);
+
+  console.log('count variable>>> ', `count${elId}`)
+  // let `count${elId}` = 8
+
+  // {1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1, 7: 1, 8: 1, 9: 1, 10: 1, 11: 1, 12: 1, 13: 1}
+
+  document.getElementById(elId).value = 2
+  // let inputVal = document.getElementById("elId")
+  // inputVal.innerHTML = 5
+  // tempCtn.innerHTML = '&'
   // .appendChild(tempCtn)
-  console.log()
+
     count++;
     event.target.value = count;
     console.log('count', count);
